28b4327e6db076d24a323b6273e1c70b998824fd
george <georgeneves.dev@gmail.com>
2023-10-11 14:26:45 -0300

    first commit

---
+++ src/projects/entities/project.entity.ts
@@ -0,0 +1,44 @@
+import crypto from 'crypto';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+export enum ProjectStatus {
+  Pending = 'pending',
+  Active = 'active',
+  Cancelled = 'cancelled',
+  Completed = 'completed',
+}
+
+@Entity()
+export class Project {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true, type: 'datetime' })
+  started_at: Date | null;
+
+  @Column({ nullable: true, type: 'datetime' })
+  cancelled_at: Date | null;
+
+  @Column({ nullable: true, type: 'datetime' })
+  forecasted_at: Date | null;
+
+  @Column({ type: 'simple-enum' })
+  status: ProjectStatus = ProjectStatus.Pending;
+
+  constructor(
+    props: {
+      name: string;
+      description: string;
+      started_at?: Date | null;
+      cancelled_at?: Date | null;
+      forecasted_at?: Date | null;
+    },
+    id?: string,
+  ) {
+    Object.assign(this, props);
+    this.id = id ?? crypto.randomUUID();
+  }
+}

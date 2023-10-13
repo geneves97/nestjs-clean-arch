4837013306c3041403895b10f74a005d977ab3d4
george <georgeneves.dev@gmail.com>
2023-10-13 11:24:33 -0300

    refactor: removendo importações sem uso

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
-import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
 import { ProjectsWhithUseCaseController } from './projects-whith-use-case.controller';
 import { CreateProjectUseCase } from './use-cases/create-project.usecase';
